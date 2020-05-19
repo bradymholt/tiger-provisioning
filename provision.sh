012b7d640ff15bcd8b0c1832104b37339a0af2da
Brady Holt <brady.holt@gmail.com>
2020-05-18 22:17:38 -0500

    Update IP

--- provision.sh
+++ provision.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --ask-vault-pass -i 192.168.1.12, -e @secrets.yml provision.yml $@
+ansible-playbook --ask-vault-pass -i 192.168.1.13, -e @secrets.yml provision.yml $@
